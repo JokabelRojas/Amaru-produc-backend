0b5807d0e516eb7290fa22057bd5f4c0c68cfbde
Felix <felyrics3@gmail.com>
2025-11-25 20:57:01 -0500

    feat(email): actualizar configuración del módulo de email para usar ConfigService

--- src/modules/email/email.module.ts
+++ src/modules/email/email.module.ts
@@ -1,22 +1,27 @@
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EmailService } from './email.service';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtp.gmail.com', 
-        port: 587,
-        secure: false,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASSWORD,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: 'smtp.gmail.com',
+          port: 587,
+          secure: false, 
+          auth: {
+            user: configService.get<string>('EMAIL_USER'),
+            pass: configService.get<string>('EMAIL_PASSWORD'),
+          },
         },
-      },
-      defaults: {
-        from: '"Amaru Producciones" <no-reply@amarproducciones.com>',
-      },
+        defaults: {
+          from: `"Amaru Producciones" <${configService.get<string>('EMAIL_USER')}>`,
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [EmailService],
